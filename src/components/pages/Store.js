524c308c7cafe5486f0f33efd41bbe73c7f7a11c
IsuruRaveen20 <isururaveen4520@gmail.com>
2022-10-19 20:10:34 +0530

    basic routing works

---
+++ src/components/pages/Store.js
@@ -0,0 +1,8 @@
+
+function Store(){
+    return(
+        <h1>Welcome to the Store</h1>
+    )
+}
+
+export default Store;
---
+++ src/components/pages/Success.js
@@ -0,0 +1,7 @@
+function Success(){
+    return(
+        <h1>Thank for your Purchase!</h1>
+    )
+}
+
+export default Success;
